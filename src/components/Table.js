4ca1405782fc155107f6c723215edff7782bc0f5
Yanni <yannidilly.prog@gmail.com>
2022-11-22 18:18:54 -0300

    implementa formatação no planetsData

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,11 +7,19 @@ function Table() {
   const [isLoading, setIsLoading] = useState(false);
   const planetsData = useContext(PlanetsDataContext);
 
+  const planetsDataFormat = (data) => {
+    const localData = data;
+    for (let index = 0; index < localData.results.length; index += 1) {
+      delete localData.results[index].residents;
+    }
+    return localData;
+  };
+
   const fetchData = async () => {
     try {
       setIsLoading(true);
       const fetchPlanetsData = await fetchPlanets();
-      planetsData.setPlanetsData(fetchPlanetsData);
+      planetsData.setPlanetsData(planetsDataFormat(fetchPlanetsData));
     } catch (error) {
       console.log(error);
     } finally {
