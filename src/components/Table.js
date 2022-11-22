1b36a825456bbc8cd2e1a07599a7d4b9c513cdc0
Yanni <yannidilly.prog@gmail.com>
2022-11-22 19:39:23 -0300

    implementa tabela criada com informações da api no estado da contextAPI

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,8 +4,10 @@ import fetchPlanets from '../services/fetchPlanets';
 import '../style/Table.css';
 
 function Table() {
-  const [isLoading, setIsLoading] = useState(false);
-  const planetsData = useContext(PlanetsDataContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const planetsDataObj = useContext(PlanetsDataContext);
+  console.log(isLoading);
+  console.log(planetsDataObj);
 
   const planetsDataFormat = (data) => {
     const localData = data;
@@ -19,7 +21,7 @@ function Table() {
     try {
       setIsLoading(true);
       const fetchPlanetsData = await fetchPlanets();
-      planetsData.setPlanetsData(planetsDataFormat(fetchPlanetsData));
+      planetsDataObj.setPlanetsData(planetsDataFormat(fetchPlanetsData));
     } catch (error) {
       console.log(error);
     } finally {
@@ -29,6 +31,7 @@ function Table() {
 
   useEffect(() => {
     fetchData();
+    console.log(planetsDataObj);
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -37,7 +40,56 @@ function Table() {
       {
         (isLoading)
           ? <div className="table-loading" />
-          : <h2>Tabela</h2>
+          : (
+            <section className="table-section">
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Rotation Period</th>
+                    <th>Orbital Period</th>
+                    <th>Diameter</th>
+                    <th>Climate</th>
+                    <th>Gravity</th>
+                    <th>Terrain</th>
+                    <th>Surface Water</th>
+                    <th>Population</th>
+                    <th>Films</th>
+                    <th>Created</th>
+                    <th>Edited</th>
+                    <th>URL</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    planetsDataObj.planetsData.results.map((planetInfo, index) => (
+                      <tr key={ index }>
+                        <td>{ planetInfo.name }</td>
+                        <td>{ planetInfo.rotation_period }</td>
+                        <td>{ planetInfo.orbital_period }</td>
+                        <td>{ planetInfo.diameter }</td>
+                        <td>{ planetInfo.climate }</td>
+                        <td>{ planetInfo.gravity }</td>
+                        <td>{ planetInfo.terrain }</td>
+                        <td>{ planetInfo.surface_water }</td>
+                        <td>{ planetInfo.population }</td>
+                        <td>
+                          {
+                            planetInfo.films.map((film, filmIndex) => (
+                              <p className="film-link" key={ filmIndex }>{ film }</p>
+                            ))
+                          }
+                        </td>
+                        <td className="film-created">{ planetInfo.created }</td>
+                        <td className="film-edited">{ planetInfo.edited }</td>
+                        <td className="planet-url">{ planetInfo.url }</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </section>
+          )
       }
     </div>
   );
