de93390fcc4650c319f8e9142b29c0218341ff1d
Yanni <yannidilly.prog@gmail.com>
2022-11-27 23:12:05 -0300

    implementa mais testes para o componente table

--- src/tests/Table.test.js
+++ src/tests/Table.test.js
@@ -18,7 +18,7 @@ test('Testa se o loading aparece e some da página quando ela é carregada', asy
   expect(loading).not.toBeInTheDocument();
 });
 
-test('Testa se a tabela muda dinamicamente de acordo com o filtro', async () => {
+test('Testa se a tabela muda dinamicamente de acordo com o filtro de nome', async () => {
   global.fetch = jest.fn(() => Promise.resolve({
     json: () => Promise.resolve(planetsData),
   }));
@@ -33,4 +33,26 @@ test('Testa se a tabela muda dinamicamente de acordo com o filtro', async () => 
   expect(alderaanText).toBeInTheDocument();
   userEvent.type(filterName, 'Tatooine');
   expect(alderaanText).not.toBeInTheDocument();
+});
+
+test('Testa se a tabela muda dinamicamente de acordo com o filtro de número', async () => {
+  global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve(planetsData),
+  }));
+
+  render(<App />);
+  const valueFilterInput = screen.queryByTestId('value-filter');
+  expect(valueFilterInput).toBeInTheDocument();
+  const filterButton = screen.queryByRole('button', { name: 'Filter' });
+  expect(filterButton).toBeInTheDocument();
+
+  const loading = screen.queryByTestId('loading');
+  await waitForElementToBeRemoved(loading);
+  expect(loading).not.toBeInTheDocument();
+
+  userEvent.type(valueFilterInput, '1000000000');
+  userEvent.click(filterButton);
+
+  let tableRows = screen.queryAllByRole('row');
+  expect(tableRows.length).toBe(4);
 });
