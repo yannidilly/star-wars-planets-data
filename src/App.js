7ef927c0351bb741014bc1c70cf847d682ebb5ed
Yanni <yannidilly.prog@gmail.com>
2022-12-07 12:32:14 -0300

    implementa gerenciamento de ordenação no estado global da aplicação

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import PlanetsDataContext from './context/PlanetsDataContext';
 import FiltersContext from './context/FiltersContext';
+import OrderContext from './context/OrderContext';
 import initialFiltersStates from './context/FiltersStates';
+import initialOrderStates from './context/OrderStates';
 import Header from './components/Header';
 import Table from './components/Table';
 import Filters from './components/Filters';
@@ -9,12 +11,15 @@ import Filters from './components/Filters';
 function App() {
   const [planetsData, setPlanetsData] = useState({});
   const [filters, setFilters] = useState(initialFiltersStates);
+  const [order, setOrder] = useState(initialOrderStates);
   return (
     <PlanetsDataContext.Provider value={ { planetsData, setPlanetsData } }>
       <FiltersContext.Provider value={ { filters, setFilters } }>
-        <Header />
-        <Filters />
-        <Table />
+        <OrderContext.Provider value={ { order, setOrder } }>
+          <Header />
+          <Filters />
+          <Table />
+        </OrderContext.Provider>
       </FiltersContext.Provider>
     </PlanetsDataContext.Provider>
   );
---
+++ src/context/OrderStates.js
@@ -0,0 +1,8 @@
+const initialOrderStates = {
+  order: {
+    column: '',
+    sort: '',
+  },
+};
+
+export default initialOrderStates;
